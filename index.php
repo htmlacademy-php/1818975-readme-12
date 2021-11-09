79133ae6ac31869c38d4768e906c449ae649a539
kirill <blinov.kir94@yandex.ru>
2021-11-09 18:05:15 +0300

    Изменил стиль оформления массива

--- index.php
+++ index.php
@@ -2,63 +2,64 @@
 $is_auth = rand(0, 1);
 
 $user_name = 'Кирилл'; // укажите здесь ваше имя
+
 $post_cards = [
     [
-    'title' => 'Цитата',
-    'type' => 'post-quote',
-    'content' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
-    'author' => 'Лариса',
-    'img' => 'userpic-larisa-small.jpg'
+        'title' => 'Цитата',
+        'type' => 'post-quote',
+        'content' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
+        'author' => 'Лариса',
+        'img' => 'userpic-larisa-small.jpg'
     ],
     [
-    'title' => 'Игра престолов',
-    'type' => 'post-text',
-    'content' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
-    'author' => 'Владик',
-    'img' => 'userpic.jpg'
+        'title' => 'Игра престолов',
+        'type' => 'post-text',
+        'content' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
+        'author' => 'Владик',
+        'img' => 'userpic.jpg'
     ],
     [
-    'title' => 'Игра престолов',
-    'type' => 'post-text',
-    'content' => 'Не могу дождаться начала финального сезона своего любимого сериала! Не могу дождаться начала финального сезона своего любимого сериала! Не могу дождаться начала финального сезона своего любимого сериала! Не могу дождаться начала финального сезона своего любимого сериала! Не могу дождаться начала финального сезона своего любимого сериала! ',
-    'author' => 'Владик',
-    'img' => 'userpic.jpg'
+        'title' => 'Игра престолов',
+        'type' => 'post-text',
+        'content' => 'Не могу дождаться начала финального сезона своего любимого сериала! Не могу дождаться начала финального сезона своего любимого сериала! Не могу дождаться начала финального сезона своего любимого сериала! Не могу дождаться начала финального сезона своего любимого сериала! Не могу дождаться начала финального сезона своего любимого сериала! ',
+        'author' => 'Владик',
+        'img' => 'userpic.jpg'
     ],
     [
-    'title' => 'Наконец, обработал фотки!',
-    'type' => 'post-photo',
-    'content' => 'rock-medium.jpg',
-    'author' => 'Виктор',
-    'img' => 'userpic-mark.jpg'
+        'title' => 'Наконец, обработал фотки!',
+        'type' => 'post-photo',
+        'content' => 'rock-medium.jpg',
+        'author' => 'Виктор',
+        'img' => 'userpic-mark.jpg'
     ],
     [
-    'title' => 'Моя мечта',
-    'type' => 'post-photo',
-    'content' => 'coast-medium.jpg',
-    'author' => 'Лариса',
-    'img' => 'userpic-larisa-small.jpg'
+        'title' => 'Моя мечта',
+        'type' => 'post-photo',
+        'content' => 'coast-medium.jpg',
+        'author' => 'Лариса',
+        'img' => 'userpic-larisa-small.jpg'
     ],
     [
-    'title' => 'Лучшие курсы',
-    'type' => 'post-link',
-    'content' => 'www.htmlacademy.ru',
-    'author' => 'Владик',
-    'img' => 'userpic.jpg'
+        'title' => 'Лучшие курсы',
+        'type' => 'post-link',
+        'content' => 'www.htmlacademy.ru',
+        'author' => 'Владик',
+        'img' => 'userpic.jpg'
     ]
 ];
 // первый вариант функции (использует 2 переменные и меньше итераций)
 function format_text1($str,$length){
     if(iconv_strlen($str) <= $length){ //Использовал функцию iconv_strlen в место strlen
-        return '<p>' . $str . '</p>';  
-    } 
+        return '<p>' . $str . '</p>';
+    }
     $words_arr = explode(' ',$str);
     $index = count($words_arr) - 1;
     for ($i = $index; $i != 0; $i--){ // считаем длину и удаляем по одноиу слову с конца
         unset($words_arr[$i]);
         if(iconv_strlen(implode(' ', $words_arr)) <= $length){
             return '<p>' . implode(' ', $words_arr) . '... </p><a class="post-text__more-link" href="#">Читать далее</a>';
-        }     
-    }   
+        }
+    }
 }
 
 ?>
@@ -127,7 +128,7 @@ function format_text1($str,$length){
                             </div>
                             <div class="header__profile-name">
                                 <span>
-                                    <?= $user_name ?> 
+                                    <?= $user_name ?>
                                 </span>
                                 <svg class="header__link-arrow" width="10" height="6">
                                     <use xlink:href="#icon-arrow-right-ad"></use>
@@ -308,7 +309,7 @@ function format_text1($str,$length){
                         </a>
                     </div>
                     <?php endif; ?>
-                </div> 
+                </div>
                 <footer class="post__footer">
                     <div class="post__author">
                         <a class="post__author-link" href="#" title="Автор">
